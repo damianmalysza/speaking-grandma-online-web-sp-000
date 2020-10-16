ce2f92105b18fa755faacde9a069f95d986ac3e1
Damian Malysza <damian.malysza@gmail.com>
2020-10-16 00:28:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,5 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(phrase)
   if phrase == phrase.uppercase
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  else
     return "HUH?! SPEAK UP, SONNY!"
 end
